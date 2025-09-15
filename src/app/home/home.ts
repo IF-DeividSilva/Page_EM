eb4c6c770cd02f05c56d0bd889653040be0566b6
Deivid da Silva Galvão <92928385+IF-DeividSilva@users.noreply.github.com>
2025-09-14 22:59:18 -0300

    cc

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
 export class HomeComponent {
-  // ... lógica do componente aqui
+  // seu código aqui
 }
