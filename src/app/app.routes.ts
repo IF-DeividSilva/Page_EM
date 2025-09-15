87bf21d1303dcfa6897d9c49a9905ebf1c6d4adc
Deivid da Silva Galvão <92928385+IF-DeividSilva@users.noreply.github.com>
2025-09-14 22:52:10 -0300

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';  // Note que mudamos de .component para apenas .ts
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent  // Substitua pelo seu componente principal
+    component: HomeComponent
   }
 ];
