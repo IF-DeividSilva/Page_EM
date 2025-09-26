25b98466b841ba21a99c4b09a7be453efe4ed25c
Deivid da Silva Galvão <92928385+IF-DeividSilva@users.noreply.github.com>
2025-09-26 14:29:50 -0300

    test page 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { Routes } from '@angular/router';
 import { Pagina1Component } from './pagina1-component/pagina1-component';
 
 
-export const routeConfig: Routes = [
-   {path: 'pagina1-component', component: Pagina1Component, title:'Pagina 1'}
+export const routes: Routes = [
+  { path: '', redirectTo: 'pagina1-component', pathMatch: 'full' },
+  { path: 'pagina1-component', component: Pagina1Component, title: 'Pagina 1' }
 ];
 
 
-export default routeConfig;
+export default routes;
