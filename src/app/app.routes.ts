1e72eade4031a3c6c8017cce9f4b8d71675ac976
Deivid da Silva Galvão <92928385+IF-DeividSilva@users.noreply.github.com>
2025-09-26 16:17:14 -0300

    update test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { Pagina1Component } from './pagina1-component/pagina1-component';
-
+import { HomeComponent } from './home-component/home-component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'pagina1-component', pathMatch: 'full' },
+  { path: '', redirectTo: 'home-component', pathMatch: 'full' },
+  { path: 'home-component', component: HomeComponent, title: 'HomePage' },
   { path: 'pagina1-component', component: Pagina1Component, title: 'Pagina 1' }
 ];
 
