ea39dea63c189ab0b997d3a2c371f8d2a1ec21a8
Deivid da Silva Galvão <92928385+IF-DeividSilva@users.noreply.github.com>
2025-09-25 23:27:53 -0300

    up page one

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Pagina1Component } from './pagina1-component/pagina1-component';
+
+
+export const routeConfig: Routes = [
+   {path: 'pagina1-component', component: Pagina1Component, title:'Pagina 1'}
+];
+
+
+export default routeConfig;
